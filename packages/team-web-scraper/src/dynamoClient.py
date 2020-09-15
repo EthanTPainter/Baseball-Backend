2a714eb6310cd4ef5b3146f147a18b2a153fa328
Ethan Painter <ethantpainter@gmail.com>
2020-09-15 10:33:01 -0400

    Completes updateTeamRecord function for dynamo record updates

--- packages/team-web-scraper/src/dynamoClient.py
+++ packages/team-web-scraper/src/dynamoClient.py
@@ -14,11 +14,29 @@ def getTeamRecord(id, tableName):
 def updateTeamRecord(id, tableName, newRecord):
   dynamodb = boto3.resource("dynamodb")
   table = dynamodb.Table(tableName)
-  record = table.get_item(
+  record = table.update_item(
     Key = {
       "partitionKey": id
     },
-    UpdateExpression = "",
+    ExpressionAttributeNames = {
+      "#W": "Wins",
+      "#L": "Losses",
+      "#DR": "DivisionRank",
+      "#G": "GamesPlayed",
+      "#H": "Hitting",
+      "#P": "Pitching",
+      "#F": "Fielding"
+    },
+    ExpressionAttributeValues = {
+      ":w": newRecord["Wins"],
+      ":l": newRecord["Losses"],
+      ":dr": newRecord["DivisionRank"],
+      ":g": newRecord["GamesPlayed"],
+      ":h": newRecord["Hitting"],
+      ":p": newRecord["Pitching"],
+      ":f": newRecord["Fielding"]
+    },
+    UpdateExpression = "SET #W = :w, #L = :l, #DR = :dr, #G = :g, #H = :h, #P = :p, #F = :f",
     ReturnConsumedCapacity = "NONE"
   )
   return record
